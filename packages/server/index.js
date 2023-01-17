a5ea197e0344210ba55a8ab27f42452e0c253ef1
Fikri Rahmat Nurhidayat <fikrirnurhidayat@gmail.com>
2023-01-17 20:16:20 +0700

    Add PDF Generation with Image.

--- packages/server/index.js
+++ packages/server/index.js
@@ -4,6 +4,7 @@ const path = require("path");
 const cors = require("cors");
 const perlin = require("perlin-noise");
 const { createCanvas } = require("canvas");
+const { legacyCreatePDF, createImage, createPDF } = require("./lib/canvas");
 
 const app = express();
 const staticDir = path.join(__dirname, "../client/dist");
@@ -27,56 +28,31 @@ app.get("/", async (req, res) => {
   res.status(200).end();
 });
 
-// TODO: It will always render A4
+app.post("/noises:random.pdf", async (req, res) => {
+  const canvas = createPDF();
+
+  res.setHeader("content-type", "application/pdf");
+  res.status(200).send(canvas.toBuffer("application/pdf"));
+});
+
 app.get("/noises:random.pdf", async (req, res) => {
-  const pageCount = Number(req.query["page_count"]) || PDF_PAGE_COUNT;
-  const noiseHeight = Number(req.query.height) || PDF_NOISE_HEIGHT;
-  const noiseWidth = Number(req.query.width) || PDF_NOISE_WIDTH;
-  const cellSize = Number(req.query["cell_size"]) || PDF_CELL_SIZE;
-  const cellWidth = Math.round(noiseWidth / cellSize);
-  const cellHeight = Math.round(noiseHeight / cellSize);
-  const canvas = createCanvas(noiseWidth, noiseHeight, "pdf");
-  const ctx = canvas.getContext("2d");
-
-  for (let i = 0; i < pageCount; i++) {
-    const noises = perlin.generatePerlinNoise(cellWidth, cellHeight);
-
-    for (let y = 0; y < cellHeight; y++) {
-      for (let x = 0; x < cellWidth; x++) {
-        const noise = noises[(x + 1) * (y + 1) - 1];
-        const color = Math.round(255 * noise);
-        const rgb = [color, color, color];
-        ctx.fillStyle = `rgb(${rgb.join(",")})`;
-        ctx.fillRect(x * cellSize, y * cellSize, cellSize, cellSize);
-      }
-    }
-
-    if (i !== pageCount - 1) ctx.addPage();
-  }
+  const canvas = legacyCreatePDF({
+    noiseHeight: Number(req.query.height),
+    noiseWidth: Number(req.query.width),
+    cellSize: Number(req.query.cell_size),
+    pageCount: Number(req.query.page_count),
+  });
 
   res.setHeader("content-type", "application/pdf");
   res.status(200).send(canvas.toBuffer("application/pdf"));
 });
 
 app.get("/noises:random", async (req, res) => {
-  const noiseHeight = Number(req.query.height) || NOISE_HEIGHT;
-  const noiseWidth = Number(req.query.width) || NOISE_WIDTH;
-  const cellSize = Number(req.query["cell_size"]) || CELL_SIZE;
-  const cellWidth = Math.round(noiseWidth / cellSize);
-  const cellHeight = Math.round(noiseHeight / cellSize);
-  const noises = perlin.generatePerlinNoise(cellWidth, cellHeight);
-  const canvas = createCanvas(noiseWidth, noiseHeight);
-  const ctx = canvas.getContext("2d");
-
-  for (let y = 0; y < cellHeight; y++) {
-    for (let x = 0; x < cellWidth; x++) {
-      const noise = noises[(x + 1) * (y + 1) - 1];
-      const color = Math.round(255 * noise);
-      const rgb = [color, color, color];
-      ctx.fillStyle = `rgb(${rgb.join(",")})`;
-      ctx.fillRect(x * cellSize, y * cellSize, cellSize, cellSize);
-    }
-  }
+  const canvas = createImage({
+    noiseHeight: Number(req.query.height),
+    noiseWidth: Number(req.query.width),
+    cellSize: Number(req.query.cell_size),
+  });
 
   res.setHeader("content-type", "image/png");
   res.status(200).send(canvas.toBuffer("image/png"));
